3865f1af52d82e5da6df6c300ff9bbe3ec529162
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-05-07 16:27:32 +0100

    controller of the second page

--- src/controller/ChatbotController.php
+++ src/controller/ChatbotController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
 class ChatbotController extends AbstractController
 {   #[Route('/chatbot', name: 'chatbot')]
     public function index()
