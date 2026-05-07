e0474e40146d39bcedc98a5f060c0b67cefd197f
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-05-07 16:28:10 +0100

    update

--- src/controller/MathematicianController.php
+++ src/controller/MathematicianController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class MathematicianController extends AbstractController
 {
     #[Route('/mathematicians', name: 'mathematicians')]
